d0a52239416cf851c249a59ef34c8f74abcf2bd8
johnngure <johnwngure@gmail.com>
2023-03-17 22:12:15 +0300

    morelargestArray

--- largestArray.py
+++ largestArray.py
@@ -1,15 +1,9 @@
 def largest(arr, n):
-    mx = arr[0]
-    for i in range (1, n):
-        if arr[1] > mx:
-            mx = arr[1]
-
-        return mx
-        
-arr = [10, 34, 45, 90, 9809]
+ 
+    return max(arr)
+ 
+# driver code
+arr = [10, 324, 45, 90, 9808]
 n = len(arr)
-
-Ans = largest(arr, n)
-
-print ("Largest in given array is",Ans)
-        
+ 
+print(largest(arr, n))
