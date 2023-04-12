106ef547cd6fa75b397e4d4ec6b61ad6163efecc
johnngure <johnwngure@gmail.com>
2023-04-12 05:50:53 +0300

    StatisticalPrinciple

---
+++ import statistics.py
@@ -0,0 +1,8 @@
+import statistics
+data = [2.75, 1.75, 1.25, 0.25, 0.5, 1.25, 3.5]
+y = statistics.median(data)
+print(y)
+z = statistics.variance(data)
+print(z)
+x = statistics.mean(data)
+print(x)
